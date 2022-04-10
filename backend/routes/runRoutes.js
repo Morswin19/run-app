89345f49eb052144d3a46ffaf1f3705995926466
Morswin19 <piotr.kalman85@gmail.com>
2022-04-10 15:56:32 +0200

    backup commit

---
+++ backend/routes/runRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const {getRuns, setRun, updateRun, deleteRun} = require('../controllers/runController')
+
+router.route('/').get(getRuns).post(setRun)
+
+router.route('/:id').put(updateRun).delete(deleteRun)
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,17 @@
-console.log('hello world')
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware')
+const port = process.env.PORT || 5000
+
+const app = express()
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+// middlewareEnd
+
+app.use('/api/runs', require('./routes/runRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server startet on port ${port}`))
